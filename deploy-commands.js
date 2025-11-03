3d93c73840e14f74960aed085ee9282591fe72fc
mikancat10 <216382990+mikancat-taki@users.noreply.github.com>
2025-11-03 20:33:20 +0900

    Create deploy-commands.js

---
+++ deploy-commands.js
@@ -0,0 +1,24 @@
+import 'dotenv/config';
+import { REST, Routes } from 'discord.js';
+import fs from 'fs';
+
+const commands = [];
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+  const command = await import(`./commands/${file}`);
+  commands.push(command.default.data.toJSON());
+}
+
+const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
+
+try {
+  console.log('スラッシュコマンド登録中...');
+  await rest.put(
+    Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+    { body: commands }
+  );
+  console.log('✅ 登録完了！');
+} catch (err) {
+  console.error(err);
+}
